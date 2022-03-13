a13514ce1c0423cc3b9bc034bade3370592b877e
lubava <joheunsaram@mail.ru>
2022-03-13 18:04:26 +0300

    all done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { TextField } from "@mui/material";
+import "./App.css";
+import { Link } from "react-router-dom";
+
+const BASE_URL = "https://kitsu.io/api/edge/anime?filter[text]=";
 
 function App() {
+  const [items, setItems] = useState({});
+  const handleSearch = (e) => {
+    console.log(e.target.value, "my input value");
+    fetch(BASE_URL + e.target.value)
+      .then((res) => res.json())
+      .then((value) => setItems(value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h3>My Anime App</h3>
+      <TextField
+        className="TextField"
+        label="Anime title"
+        onChange={handleSearch}
+      />
+      <div className="results">
+        {items.data &&
+          items.data.map((item, index) => (
+            <div key={index}>
+              <a href={`${item.links.self}`}>
+                {item.attributes.canonicalTitle}
+              </a>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
